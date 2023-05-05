a2370321c287cd75f44afa09bb41015c116f8482
choiruladamm <choiruladamm@gmail.com>
2023-05-05 21:02:24 +0700

    Add Events

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import DisplayingData from "./components/DisplayingData";
 import RenderingList from "./components/RenderingList";
+import RespondingEvents from "./components/RespondingEvents";
 
 const App = () => {
   return (
     <>
       <DisplayingData />
       <RenderingList />
+      <RespondingEvents />
     </>
   );
 };
---
+++ src/components/RespondingEvents.jsx
@@ -0,0 +1,15 @@
+
+
+const RespondingEvents = () => {
+  const handleClick = () => {
+    alert('Hello Gaisss')
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Cobak klik en aku
+    </button>
+  )
+}
+
+export default RespondingEvents
