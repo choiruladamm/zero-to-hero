2ee96a4d1299fee0e0bd98ca8ba2de45e8c5ce3f
choiruladamm <choiruladamm@gmail.com>
2023-05-06 23:27:48 +0700

    Follow Up

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 
 export const TicTacToe = () => {
@@ -9,36 +10,33 @@ export const TicTacToe = () => {
 };
 
 export const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
 };
 
-export const Square = () => {
-  const [value, setValue] = useState(null);
-
-  const handleClick = () => {
-    setValue("X");
-  };
+export const Square = ({value}) => {
 
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square">
       {value}
     </button>
   );
