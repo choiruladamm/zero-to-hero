0157f01af1d73d88e8fb1b3936bb2ee20284f491
choiruladamm <choiruladamm@gmail.com>
2023-06-02 14:51:56 +0700

    Refactoring Code

--- src/course/roadmapsh/components/Accordion.jsx
+++ src/course/roadmapsh/components/Accordion.jsx
@@ -1,25 +1,30 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
+
 import { Button } from "@material-tailwind/react";
-import { useState } from "react";
 import useToogle from "./hooks/useToogle";
 
-const Accordion = () => {
-  // const [expanded, setExpanded] = useState(false)
-
-  // const toogleExpanded = () => {
-  //   setExpanded((prevExpanded) => !prevExpanded)
-  // }
-
-  const {status: expanded, toogleStatus: toogleExpanded} = useToogle()
+export default function Accordion({ children, header }) {
+  const { status: expanded, toogleStatus: toogleExpanded } = useToogle();
 
   return (
     <div>
-      <Button onClick={toogleExpanded}>
-        Header <span>{expanded ? '-' : '+'}</span>
-      </Button>
-      {expanded && <div>Content</div>}
+      <AccordionHeader expanded={expanded} toogleExpanded={toogleExpanded}>
+        {header}
+      </AccordionHeader>
+      <AccordionContent expanded={expanded}>{children}</AccordionContent>
     </div>
-  )
+  );
 }
 
-export default Accordion
+function AccordionHeader({ children, expanded, toogleExpanded }) {
+  return (
+    <Button onClick={toogleExpanded}>
+      {children} <span>{expanded ? "-" : "+"}</span>
+    </Button>
+  );
+}
+
+function AccordionContent({ children, expanded }) {
+  return <>{expanded && children}</>;
+}
