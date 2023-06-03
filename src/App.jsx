d874814e813f9106047d85c777a4a2c42e62f641
choiruladamm <choiruladamm@gmail.com>
2023-06-03 22:47:31 +0700

    Add React Props Destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable no-unused-vars */
 
+import DestructuringProps from "./course/roadmapsh/components-docs/passing-props/DestructuringProps";
+
 const App = () => {
   return (
-    <div>
-      Reset
+    <div className="grid h-screen place-items-center">
+      <DestructuringProps />
     </div>
   );
 };
---
+++ src/course/roadmapsh/components-docs/passing-props/DestructuringProps.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+
+import React from "react";
+import Welcome from "./elements/Welcome";
+
+const DestructuringProps = () => {
+  return (
+    <div>
+      <Welcome>Hello Adam, Calon Software Enginer</Welcome>
+    </div>
+  );
+};
+
+export default DestructuringProps;
