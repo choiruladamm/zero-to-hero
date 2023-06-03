c7f437063d7fa012976177c24d348f8b20cd3d14
choiruladamm <choiruladamm@gmail.com>
2023-06-03 22:28:36 +0700

    Reset Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 /* eslint-disable no-unused-vars */
 
-import Accordion from "./course/roadmapsh/components/Accordion";
-
 const App = () => {
   return (
-    <div className="grid h-screen place-items-center">
-      <Accordion header="Accordion 1">
-        <div>Content 1</div>
-      </Accordion>
+    <div>
+      Reset
     </div>
   );
 };
