3d4399a311416f11e7afa93d148ed42a90e7a800
choiruladamm <choiruladamm@gmail.com>
2023-06-24 21:45:32 +0700

    Explore Tailwind-Merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 /* eslint-disable no-unused-vars */
 
-import LearnUseRefs from "./course/roadmapsh/rendering-docs/refs/LearnUseRefs";
-import RefStopWatch from "./course/roadmapsh/rendering-docs/refs/RefStopWatch";
-
-
 const App = () => {
   return (
     <div className="grid h-screen place-items-center">
-      <RefStopWatch />
+      
     </div>
   );
 };
