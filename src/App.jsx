b597165e267fa6071a10457bdf20936190462280
choiruladamm <choiruladamm@gmail.com>
2023-06-24 22:14:32 +0700

    Explore Tailwind-Merge

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,12 @@ import ButtonMerge from "./course/explore/ButtonMerge";
 export default function App() {
   return (
     <div className="grid place-items-center h-screen">
-      <ButtonMerge className="px-5 py-2 rounded-full">Hey yo</ButtonMerge>
+      <ButtonMerge
+        color="red"
+        className="px-5 py-2 font-semibold hover:border-[1.5px] hover:border-red-600 hover:bg-transparent hover:text-red-600"
+      >
+        Hey yo
+      </ButtonMerge>
     </div>
-  )
+  );
 }
-
-
--- src/course/explore/ButtonMerge.jsx
+++ src/course/explore/ButtonMerge.jsx
@@ -1,13 +1,14 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 
 import { twMerge } from "tailwind-merge";
 
-export default function ButtonMerge({ className, ...props }) {
-  const c = twMerge(`px-2 py-1 rounded-sm ${className} bg-gray-800 text-white text-sm`)
-  
+export default function ButtonMerge({ className, color = "gray", children }) {
+  const c = twMerge(
+    `px-2 py-1 rounded-sm ${className} bg-${color}-600 text-white text-sm`
+  );
+
   console.log(c);
-  
-  return (
-    <button {...props} className={c} />
-  )
+
+  return <button className={c}>{children}</button>;
 }
