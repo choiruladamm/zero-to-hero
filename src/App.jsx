1385b93868938f95646cb59d03ee33ba776c5a41
choiruladamm <choiruladamm@gmail.com>
2023-07-08 23:37:49 +0700

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 
+import { Routes, Router, Route } from "react-router-dom";
 import ReactRouter from "./course/roadmapsh/react-router/ReactRouter";
 
 const App = () => {
